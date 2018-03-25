91a1e09f7419276972bc35e087561bfaa68a46e9
Konstantin <ivaninkv@outlook.com>
2018-03-25 14:59:28 +0300

    изменил адрес

--- Realty/MainProgram.cs
+++ Realty/MainProgram.cs
@@ -7,7 +7,7 @@ namespace Realty
         static int Main(string[] args)
         {
             AddrSuggestion addrSuggestion = new AddrSuggestion(Const.Token, Const.Url);
-            addrSuggestion.GetAddrSuggestion("Московская область, м. Домодедовская, Ленинский район, д. Калиновка, 57А");
+            addrSuggestion.GetAddrSuggestion("Московская область, Ленинский район, д. Калиновка, 57А");
             Console.WriteLine(addrSuggestion.City);
             Console.WriteLine(addrSuggestion.Street);
             Console.ReadKey();          
