8ab5e54da77f30d082939f5c15ce814379148f83
Konstantin <ivaninkv@outlook.com>
2018-05-22 20:46:35 +0300

    floor

--- Realty/Avito.cs
+++ Realty/Avito.cs
@@ -14,7 +14,7 @@ namespace Realty
         /// </summary>
         /// <param name="searchurl">Поисковый запрос, который нужно распарсить.</param>
         /// <param name="delay">Задержка между запросами, с.</param>
-        public Avito(string searchurl, int delay = 4) : base(searchurl)
+        public Avito(string searchurl, int delay = 3) : base(searchurl)
         {
             Delay = delay;
             Floor = ".floor";
@@ -34,14 +34,21 @@ namespace Realty
         {
             var dt = new DataTable();
             dt = base.Parse();
+            dt.Columns.Add("FloorAll");
             foreach (DataRow row in dt.Rows)
             {                
-                row["Area"] = row["Area"].ToString().Replace("м²", String.Empty).Replace(".", ",").Trim();                
+                row["Area"] = row["Area"].ToString().Replace("м²", String.Empty).Replace(".", ",").Trim();
                 row["Price"] = row["Price"].ToString().Replace("р. в месяц", String.Empty).Replace(".", ",").Trim();
+                row["Floor"] = row["Floor"].ToString().Replace(" эт.", String.Empty).Trim();
+                row["FloorAll"] = row["Floor"].ToString().Split('/')[1];
+                row["Floor"] = row["Floor"].ToString().Split('/')[0];
+
             }
             
             dt.ChangeColumnDataType("Area", typeof(decimal));
             dt.ChangeColumnDataType("Price", typeof(decimal));
+            dt.ChangeColumnDataType("Floor", typeof(int));
+            dt.ChangeColumnDataType("FloorAll", typeof(int));
 
             return dt;
         }
